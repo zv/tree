32962402955b85f80c6f50c64c5b0358ba662f76
zv <zephyr.pellerin@gmail.com>
2021-05-06 13:29:13 -0700

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-require('babel-polyfill');
+require('@babel/polyfill');
 var path = require('path');
 var dir_js = path.resolve(__dirname, 'src');
 var dir_build = path.resolve(__dirname, 'build');
@@ -10,14 +10,6 @@ module.exports = {
         library: 'ZVTree',
         path: dir_build,
         filename: 'zv-tree.js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: dir_js,
-            }
-        ]
     },
     // Create Sourcemaps for the bundle
     devtool: 'source-map',
