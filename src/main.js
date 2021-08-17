a04773ec1b744cce25d27fb761aa7492fbcd67ca
zv <zephyr.pellerin@gmail.com>
2021-08-17 07:48:06 -0700

    Remove `render.js`

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-import {RenderTree} from './render.js'
-import {Tree} from './tree.js'
+import { Tree } from './tree.js'
 
-var canvas = document.getElementById('tree');
+var canvas = document.getElementById('tree')
 
 export var config = {
     _size: null,
     mid: 0.5,
     branch_split_diminish: 0.71,
-    branch_split_angle: 0.3*Math.PI,
+    branch_split_angle: 0.3 * Math.PI,
     branch_angle_exp: 2,
-    trunk_stroke: "black",
-    trunk: "white",
-    trunk_shade: "rgba(0,0,0,0.5)",
+    trunk_stroke: 'black',
+    trunk: 'white',
+    trunk_shade: 'rgba(0,0,0,0.5)',
 
     get size() {
         // lame
@@ -23,11 +22,11 @@ export var config = {
     },
 
     get branch_angle_max() {
-        return 5*Math.PI / this.size
+        return (5 * Math.PI) / this.size
     },
 
     get grains() {
-        return Math.ceil(this.size*0.02)
+        return Math.ceil(this.size * 0.02)
     },
 
     get branch_diminish() {
@@ -35,53 +34,52 @@ export var config = {
     },
 
     get one() {
-        return 1/this.size
+        return 1 / this.size
     },
 
     get branch_prob_scale() {
-        return 1/this.init_branch/this.size*18
-    }
+        return (1 / this.init_branch / this.size) * 18
+    },
 }
 
-
 export function draw() {
-    if (canvas.getContext){
-        var ctx = canvas.getContext('2d');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d')
     } else {
-        console.log("No canvas")
-        return -1;
+        console.log('No canvas')
+        return -1
     }
 
-    var render = new RenderTree({n: config.size,
-                                 trunk: config.trunk,
-                                 trunk_stroke: config.trunk_stroke,
-                                 grains: config.grains,
-                                 one: 1, // scaling factor
-                                 context: ctx})
-
-    var tree = new Tree({root_x: config.mid,
-                         root_y: 0.90,
-                         root_r: config.init_branch,
-                         root_a: (-1*Math.PI)*0.5,
-                         stepsize: config.one,
-                         one: config.one,
-                         branch_split_angle: config.branch_split_angle,
-                         branch_prob_scale: config.branch_prob_scale,
-                         branch_diminish: config.branch_diminish,
-                         branch_split_diminish: config.branch_split_diminish,
-                         branch_angle_max: config.branch_angle_max,
-                         branch_angle_exp: config.branch_angle_exp})
+    var tree = new Tree({
+        context: ctx,
+        root_x: config.mid,
+        root_y: 0.9,
+        root_r: config.init_branch,
+        root_a: -1 * Math.PI * 0.5,
+        stepsize: config.one,
+        one: config.one,
+        branch_split_angle: config.branch_split_angle,
+        branch_prob_scale: config.branch_prob_scale,
+        branch_diminish: config.branch_diminish,
+        branch_split_diminish: config.branch_split_diminish,
+        branch_angle_max: config.branch_angle_max,
+        branch_angle_exp: config.branch_angle_exp,
+        n: config.size,
+        trunk: config.trunk,
+        trunk_stroke: config.trunk_stroke,
+        grains: config.grains,
+    })
 
     function drawStep() {
         tree.step()
-        tree.Q.map(render.drawBranch.bind(render))
+        tree.draw(ctx)
 
         if (tree.Q.length) {
             requestAnimationFrame(drawStep)
         } else {
-            console.log("done")
+            console.log('done')
         }
     }
 
-    drawStep();
+    drawStep()
 }
--- src/tree.js
+++ src/tree.js
@@ -1,43 +1,118 @@
-import {normalDistribution, randomInteger, uniformRandom} from './utils.js'
-
+const square = (n) => n * n
+const randomInteger = (n) => Math.floor(Math.random() * n)
+const uniformRandom = (n) => (n ? randomInteger(n) : Math.random())
+const normalDistribution = () => {
+    var u = 1 - Math.random() // Subtraction to flip [0, 1) to (0, 1].
+    var v = 1 - Math.random()
+    return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+}
 
 class Branch {
     constructor(kwargs) {
-        for(let k in kwargs)
-            this[k] = kwargs[k]
+        for (let k in kwargs) this[k] = kwargs[k]
     }
 
     step() {
         this.r -= this.tree.branch_diminish
-        var angle = normalDistribution() * this.tree.branch_angle_max
-        var scale = this.tree.one + this.tree.root_r - this.r
+        const angle = normalDistribution() * this.tree.branch_angle_max
+        const scale = this.tree.one + this.tree.root_r - this.r
 
         // Calculate our Thue path and...
-        var da = Math.pow(1 + scale / this.tree.root_r, this.tree.branch_angle_exp)
-        var dx = Math.cos(this.a)*this.tree.stepsize
-        var dy = Math.sin(this.a)*this.tree.stepsize
+        const da = Math.pow(
+            1 + scale / this.tree.root_r,
+            this.tree.branch_angle_exp
+        )
+        const dx = Math.cos(this.a) * this.tree.stepsize
+        const dy = Math.sin(this.a) * this.tree.stepsize
 
         // Increment
-        this.a += da*angle
+        this.a += da * angle
         this.x += dx
         this.y += dy
     }
-}
 
+    draw(context) {
+        const { a, r, x, y } = this
+
+        const scale = this.tree.n
+
+        const x1 = x + Math.cos(a - 0.5 * Math.PI) * r,
+            x2 = x + Math.cos(a + 0.5 * Math.PI) * r,
+            y1 = y + Math.sin(a - 0.5 * Math.PI) * r,
+            y2 = y + Math.sin(a + 0.5 * Math.PI) * r
+
+        let ctx = context
+
+        // Make our line white & one pixel wide
+        ctx.strokeStyle = this.tree.trunk
+        ctx.lineWidth = 2
+        ctx.fillStyle = this.tree.trunk_stroke
+
+        // Fill in a pixel on the canvas
+        const fillPixel = (x, y) => fillRectN(x, y, 1)
+
+        // Fill in a square of size N
+        const fillRectN = (x, y, n) => ctx.fillRect(x * scale, y * scale, n, n)
+
+        const fillLine = (start, end) => {
+            ctx.beginPath()
+            ctx.moveTo(scale * start.x, scale * start.y)
+            ctx.lineTo(scale * end.x, scale * end.y)
+            ctx.stroke()
+        }
+
+        fillLine(
+            { x: x1, y: y1 }, // start
+            { x: x2, y: y2 } // end
+        )
+
+        // Create our outline
+        fillPixel(x1, y1)
+        fillPixel(x2, y2)
+
+        const makeTrunk = (xl, yl, the, dd, len) => {
+            for (let i = 0; i <= len; i++) {
+                let s = Math.random() * Math.random() * dd
+                fillPixel(xl - s * Math.cos(the), yl - s * Math.sin(the))
+            }
+        }
+
+        // Trunk shade right
+        makeTrunk(
+            x2,
+            y2,
+            0.5 * Math.PI + a, // val
+            Math.sqrt(square(x - x2) + square(y - y2)), // dd
+            this.tree.grains // len
+        )
+
+        // Trunk shade left
+        makeTrunk(
+            x1,
+            y1,
+            a - 0.5 * Math.PI, // val
+            Math.sqrt(square(x - x1) + square(y - y1)), // dd
+            this.tree.grains / 5 // len
+        )
+    }
+}
 
 export class Tree {
     constructor(kwargs) {
-        for(let k in kwargs) {
+        for (let k in kwargs) {
             this[k] = kwargs[k]
         }
 
         this.Q = [] // All of the tree's branches
-        this.Q.push(new Branch({tree: this,
-                                x: this.root_x,
-                                y: this.root_y,
-                                r: this.root_r,
-                                a: this.root_a,
-                                g: 0}))
+        let branch = new Branch({
+            tree: this,
+            x: this.root_x,
+            y: this.root_y,
+            r: this.root_r,
+            a: this.root_a,
+            g: 0,
+        })
+        this.Q.push(branch)
         console.log(this)
     }
 
@@ -46,7 +121,7 @@ export class Tree {
      */
     step() {
         var q_remove = [] // What we will prune
-        var q_new = []    // Any new branches we might grow
+        var q_new = [] // Any new branches we might grow
 
         for (let [i, branch] of this.Q.entries()) {
             branch.step() // Grow our branch
@@ -57,13 +132,26 @@ export class Tree {
                 continue
             }
 
+            let branch_prob =
+                (this.root_r - branch.r + this.one) * this.branch_prob_scale
+
             // Now, roll the dice and create a new branch if we're lucky
-            let branch_prob = (this.root_r - branch.r + this.one) * this.branch_prob_scale
             if (uniformRandom() < branch_prob) {
-                let {x,y,a,r,g} = branch
-                let new_r = this.branch_split_diminish*r
-                let ra = (Math.pow(-1, randomInteger(2)) * uniformRandom()) * this.branch_split_angle
-                q_new.push(new Branch({tree: this, x: x, y: y, r: new_r, a: a + ra, g: g + 1}))
+                let ra =
+                    Math.pow(-1, randomInteger(2)) *
+                    uniformRandom() *
+                    this.branch_split_angle
+
+                q_new.push(
+                    new Branch({
+                        tree: this,
+                        x: branch.x,
+                        y: branch.y,
+                        r: this.branch_split_diminish * branch.r,
+                        a: branch.a + ra,
+                        g: branch.g + 1,
+                    })
+                )
             } else {
                 // Otherwise just keep growing our branch!
                 q_remove.push(i)
@@ -78,5 +166,9 @@ export class Tree {
         }
 
         this.Q = this.Q.concat(q_new)
+    }
+
+    draw(context) {
+        this.Q.map((branch) => branch.draw(context))
     }
 }
